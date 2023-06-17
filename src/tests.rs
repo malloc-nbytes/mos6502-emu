bc966b059e06c7f9cf12ea592bdf1e4e48758878
malloc-nbytes <zdhdev@yahoo.com>
2023-06-17 17:22:32 -0500

    Rewrote lda tests

--- src/tests.rs
+++ src/tests.rs
@@ -12,13 +12,15 @@ mod tests {
         for (addr, opcode) in instrs {
             *mem.at(addr as usize) = opcode;
         }
-        Mos6502::new(mem)
+        let mut cpu = Mos6502::new(mem);
+        cpu.reset();
+        cpu
     }
 
-    fn assert_all_status_flags_false_except(cpu: &Mos6502, excluded_flag: Option<Mos6502Flags>) {
+    fn assert_all_status_flags_false_except(cpu: &Mos6502, excluded_flags: Vec<Mos6502Flags>) {
         let assert_flag_state = |flag: bool, flag_name: &str, current_flag: Mos6502Flags| {
-            if Some(current_flag) != excluded_flag {
-                assert!(!flag, "Failed assertion for flag: {flag_name}");
+            if !excluded_flags.contains(&current_flag) {
+                assert!(!flag, "Failed assertion for flag: {}", flag_name);
             }
         };
 
@@ -35,30 +37,46 @@ mod tests {
     ////////// LDA //////////
 
     #[test]
-    fn lda_imm() {
+    fn illegal_opcode_wcycles() {
         let mut cpu = cpu_mem_set(vec![
-            (0xFFFC, instructions::LDA_IMM),
-            (0xFFFD, 0x32),
+            (0xFFFC, 0x02),
+            (0xFFFD, 0x02),
         ]);
-        cpu.reset();
+
         cpu.exe(Some(instructions::LDA_IMM_CCOST));
-        assert_eq!(cpu.get_accumulator(), 0x32);
-        assert_all_status_flags_false_except(&cpu, Some(Mos6502Flags::N));
-        assert_eq!(cpu.get_cycles(), instructions::LDA_IMM_CCOST); 
+        assert_eq!(cpu.get_cycles(), instructions::LDA_IMM_CCOST);
     }
 
     #[test]
-    fn lda_imm_wzero_flag() {
+    fn lda_imm() {
+        let mut cpu = cpu_mem_set(vec![
+            (0xFFFC, instructions::LDA_IMM),
+            (0xFFFD, 0x84),
+        ]);
+
+        cpu.exe(Some(instructions::LDA_IMM_CCOST));
+
+        assert_eq!(cpu.get_accumulator(), 0x84);
+        assert_eq!(cpu.get_cycles(), instructions::LDA_IMM_CCOST);
+
+        assert_all_status_flags_false_except(&cpu, vec![Mos6502Flags::N]);
+        assert!(cpu.negative_flag());
+    }
+
+    #[test]
+    fn lda_imm_wzero() {
         let mut cpu = cpu_mem_set(vec![
             (0xFFFC, instructions::LDA_IMM),
             (0xFFFD, 0x00),
         ]);
-        cpu.reset();
+
+        cpu.set_xreg(0x44);
         cpu.exe(Some(instructions::LDA_IMM_CCOST));
-        assert_eq!(cpu.get_accumulator(), 0x00);
-        assert_all_status_flags_false_except(&cpu, Some(Mos6502Flags::Z));
-        assert!(cpu.zero_flag());
+
         assert_eq!(cpu.get_cycles(), instructions::LDA_IMM_CCOST);
+
+        assert_all_status_flags_false_except(&cpu, vec![Mos6502Flags::Z]);
+        assert!(cpu.zero_flag());
     }
 
     #[test]
@@ -66,14 +84,13 @@ mod tests {
         let mut cpu = cpu_mem_set(vec![
             (0xFFFC, instructions::LDA_ZP),
             (0xFFFD, 0x42),
-            (0x0042, 0x80),
+            (0x0042, 0x37),
         ]);
-        cpu.reset();
+
         cpu.exe(Some(instructions::LDA_ZP_CCOST));
-        assert_eq!(cpu.get_accumulator(), 0x80);
-        assert_all_status_flags_false_except(&cpu, Some(Mos6502Flags::N));
-        assert!(cpu.negative_flag());
-        assert_eq!(cpu.get_cycles(), instructions::LDA_ZP_CCOST);
+
+        assert_eq!(cpu.get_accumulator(), 0x37);
+        assert_all_status_flags_false_except(&cpu, vec![]);
     }
 
     #[test]
@@ -83,12 +100,12 @@ mod tests {
             (0xFFFD, 0x42),
             (0x0047, 0x37),
         ]);
-        cpu.reset();
+
         cpu.set_xreg(5);
         cpu.exe(Some(instructions::LDA_ZPX_CCOST));
+
         assert_eq!(cpu.get_accumulator(), 0x37);
-        assert_all_status_flags_false_except(&cpu, None);
-        assert_eq!(cpu.get_cycles(), instructions::LDA_ZPX_CCOST);
+        assert_all_status_flags_false_except(&cpu, vec![]);
     }
 
     #[test]
@@ -98,110 +115,100 @@ mod tests {
             (0xFFFD, 0x80),
             (0x007F, 0x37),
         ]);
-        cpu.reset();
+
         cpu.set_xreg(0xFF);
         cpu.exe(Some(instructions::LDA_ZPX_CCOST));
+
         assert_eq!(cpu.get_accumulator(), 0x37);
-        assert_all_status_flags_false_except(&cpu, None);
-        assert_eq!(cpu.get_cycles(), instructions::LDA_ZPX_CCOST);
+        assert_all_status_flags_false_except(&cpu, vec![]);
     }
 
     #[test]
     fn lda_abs() {
-        assert!(false);
-    }
-
-    #[test]
-    fn lda_imm_zero_flag() {
         let mut cpu = cpu_mem_set(vec![
-            (0xFFFC, instructions::LDA_IMM),
-            (0xFFFD, 0x00),
+            (0xFFFC, instructions::LDA_ABS),
+            (0xFFFD, 0x80),
+            (0xFFFE, 0x44), // 4480
+            (0x4480, 0x37),
         ]);
 
-        cpu.reset();
-        cpu.set_accumulator(0x44);
-        cpu.exe(Some(instructions::LDA_IMM_CCOST));
+        cpu.exe(Some(instructions::LDA_ABS_CCOST));
 
-        assert_all_status_flags_false_except(&cpu, Some(Mos6502Flags::Z));
-        assert!(cpu.zero_flag());
-        assert_eq!(cpu.get_cycles(), instructions::LDA_IMM_CCOST); 
-        assert_eq!(cpu.get_accumulator(), 0x00);
+        assert_eq!(cpu.get_accumulator(), 0x37);
+        assert_eq!(cpu.get_cycles(), instructions::LDA_ABS_CCOST);
+        assert_all_status_flags_false_except(&cpu, vec![]);
     }
 
     #[test]
-    fn lda_absx_page_crossed() {
+    fn lda_absx_wopage_boundary() {
+        let mut cpu = cpu_mem_set(vec![
+            (0xFFFC, instructions::LDA_ABSX),
+            (0xFFFD, 0x80),
+            (0xFFFE, 0x44), // 4480
+            (0x4481, 0x37),
+        ]);
+
+        cpu.set_xreg(1);
+        cpu.exe(Some(instructions::LDA_ABSX_CCOST));
+
+        assert_eq!(cpu.get_accumulator(), 0x37);
+        assert_eq!(cpu.get_cycles(), instructions::LDA_ABSX_CCOST);
+
+        assert_all_status_flags_false_except(&cpu, vec![]);
+    }
+
+    #[test]
+    fn lda_absx_wpage_boundary() {
         let mut cpu = cpu_mem_set(vec![
             (0xFFFC, instructions::LDA_ABSX),
             (0xFFFD, 0x02),
             (0xFFFE, 0x44), // 0x4402
-            (0x4501, 0x37), // 0x4402 + 0xFF crosses page boundary.
+            (0x4501, 0x37), // 0x4402 + 0xFF crosses page boundary
         ]);
 
-        cpu.reset();
         cpu.set_xreg(0xFF);
-        cpu.exe(Some(instructions::LDA_ABSX_CCOST));
+        cpu.exe(Some(instructions::LDA_ABSX_CCOST + 1));
 
-        assert_all_status_flags_false_except(&cpu, Some(Mos6502Flags::Z));
-        assert!(cpu.zero_flag());
+        assert_eq!(cpu.get_accumulator(), 0x37);
         assert_eq!(cpu.get_cycles(), instructions::LDA_ABSX_CCOST + 1);
-        assert_eq!(cpu.get_accumulator(), 0x00);
+
+        assert_all_status_flags_false_except(&cpu, vec![]);
     }
 
     #[test]
-    fn lda_absx_no_page_crossed() {
+    fn lda_absy_wopage_boundary() {
         let mut cpu = cpu_mem_set(vec![
-            (0xFFFC, instructions::LDA_ABSX),
+            (0xFFFC, instructions::LDA_ABSY),
             (0xFFFD, 0x80),
-            (0xFFFE, 0x44), // 0x4480
+            (0xFFFE, 0x44), // 4480
             (0x4481, 0x37),
         ]);
 
-        cpu.reset();
-        cpu.set_xreg(1u8);
-        cpu.exe(Some(instructions::LDA_ABSX_CCOST));
+        cpu.set_yreg(1);
+        cpu.exe(Some(instructions::LDA_ABSY_CCOST));
 
-        assert_all_status_flags_false_except(&cpu, Some(Mos6502Flags::Z));
-        assert!(cpu.zero_flag());
-        assert_eq!(cpu.get_cycles(), instructions::LDA_ABSX_CCOST); 
-        assert_eq!(cpu.get_accumulator(), 0x00);
+        assert_eq!(cpu.get_accumulator(), 0x37);
+        assert_eq!(cpu.get_cycles(), instructions::LDA_ABSY_CCOST);
+
+        assert_all_status_flags_false_except(&cpu, vec![]);
     }
 
     #[test]
-    fn lda_absy_page_crossed() {
+    fn lda_absy_wpage_boundary() {
         let mut cpu = cpu_mem_set(vec![
             (0xFFFC, instructions::LDA_ABSY),
             (0xFFFD, 0x02),
             (0xFFFE, 0x44), // 0x4402
-            (0x4501, 0x37), // 0x4402 + 0xFF crosses page boundary.
+            (0x4501, 0x37), // 0x4402 + 0xFF crosses page boundary
         ]);
 
-        cpu.reset();
         cpu.set_yreg(0xFF);
-        cpu.exe(Some(instructions::LDA_ABSY_CCOST));
+        cpu.exe(Some(instructions::LDA_ABSY_CCOST + 1));
 
-        assert_all_status_flags_false_except(&cpu, Some(Mos6502Flags::Z));
-        assert!(cpu.zero_flag());
-        assert_eq!(cpu.get_cycles(), instructions::LDA_ABSY_CCOST + 1); 
-        assert_eq!(cpu.get_accumulator(), 0x00);
-    }
+        assert_eq!(cpu.get_accumulator(), 0x37);
+        assert_eq!(cpu.get_cycles(), instructions::LDA_ABSY_CCOST + 1);
 
-    #[test]
-    fn lda_absy_no_page_crossed() {
-        let mut cpu = cpu_mem_set(vec![
-            (0xFFFC, instructions::LDA_ABSY),
-            (0xFFFD, 0x80),
-            (0xFFFE, 0x44), // 0x4480
-            (0x4481, 0x37),
-        ]);
-
-        cpu.reset();
-        cpu.set_yreg(1u8);
-        cpu.exe(Some(instructions::LDA_ABSY_CCOST));
-
-        assert_all_status_flags_false_except(&cpu, Some(Mos6502Flags::Z));
-        assert!(cpu.zero_flag());
-        assert_eq!(cpu.get_cycles(), instructions::LDA_ABSY_CCOST); 
-        assert_eq!(cpu.get_accumulator(), 0x00);
+        assert_all_status_flags_false_except(&cpu, vec![]);
     }
 
     #[test]
@@ -209,25 +216,62 @@ mod tests {
         let mut cpu = cpu_mem_set(vec![
             (0xFFFC, instructions::LDA_ZPX_IND),
             (0xFFFD, 0x02),
-            (0x0006, 0x00), // 0x02 + 0x04 (xreg) = 0x0006
+            (0x0006, 0x00), // 0x0006 = 0x02 + 0x04 (xreg)
             (0x0007, 0x80),
             (0x8000, 0x37),
         ]);
-        cpu.reset();
+
         cpu.set_xreg(0x04);
         cpu.exe(Some(instructions::LDA_ZPX_IND_CCOST));
 
-        // assert_all_status_flags_false_except(&cpu, Some(Mos6502Flags::Z));
-        assert!(!cpu.zero_flag());
-        assert!(!cpu.negative_flag());
-        assert_eq!(cpu.get_cycles(), instructions::LDA_ZPX_IND_CCOST); 
         assert_eq!(cpu.get_accumulator(), 0x37);
+        assert_eq!(cpu.get_cycles(), instructions::LDA_ZPX_IND_CCOST);
+
+        assert_all_status_flags_false_except(&cpu, vec![]);
+    }
+
+    #[test]
+    fn lda_zpy_ind_wopage_boundary() {
+        let mut cpu = cpu_mem_set(vec![
+            (0xFFFC, instructions::LDA_ZPY_IND),
+            (0xFFFD, 0x02),
+            (0x0002, 0x00),
+            (0x0003, 0x80),
+            (0x8004, 0x37), // 0x8004 = 0x8000 + 0x04 (yreg)
+        ]);
+
+        cpu.set_yreg(0x04);
+        cpu.exe(Some(instructions::LDA_ZPY_IND_CCOST));
+
+        assert_eq!(cpu.get_accumulator(), 0x37);
+        assert_eq!(cpu.get_cycles(), instructions::LDA_ZPY_IND_CCOST);
+
+        assert_all_status_flags_false_except(&cpu, vec![]);
+    }
+
+    #[test]
+    fn lda_zpy_ind_wpage_boundary() {
+        let mut cpu = cpu_mem_set(vec![
+            (0xFFFC, instructions::LDA_ZPY_IND),
+            (0xFFFD, 0x02),
+            (0x0002, 0x02),
+            (0x0003, 0x80),
+            (0x8101, 0x37), // 0x8101 = 0x8002 + 0xFF (yreg)
+        ]);
+
+        cpu.set_yreg(0xFF);
+        cpu.exe(Some(instructions::LDA_ZPY_IND_CCOST + 1));
+
+        assert_eq!(cpu.get_accumulator(), 0x37);
+        assert_eq!(cpu.get_cycles(), instructions::LDA_ZPY_IND_CCOST + 1);
+
+        assert_all_status_flags_false_except(&cpu, vec![]);
     }
 
     ////////// JSR //////////
 
     #[test]
-    fn jsr_abs() {
+    fn jsr_abs_wlda_imm() {
         let mut cpu = cpu_mem_set(vec![
             (0xFFFC, instructions::JSR_ABS),
             (0xFFFD, 0x42),
@@ -235,12 +279,11 @@ mod tests {
             (0x4242, instructions::LDA_IMM),
             (0x4243, 0x84),
         ]);
-        cpu.reset();
+
         cpu.exe(Some(instructions::JSR_ABS_CCOST + instructions::LDA_IMM_CCOST));
+
         assert_eq!(cpu.get_accumulator(), 0x84);
-        // Excluding negative flag because of the instruction
-        // LDA_IMM being put into memory.
-        assert_all_status_flags_false_except(&cpu, Some(Mos6502Flags::N));
+        assert_all_status_flags_false_except(&cpu, vec![Mos6502Flags::N]);
         assert!(cpu.negative_flag());
         assert_eq!(cpu.get_cycles(), instructions::JSR_ABS_CCOST + instructions::LDA_IMM_CCOST);
     }
@@ -252,9 +295,8 @@ mod tests {
         let mut cpu = cpu_mem_set(vec![
             (0xFFFC, instructions::NOP_IMP),
         ]);
-        cpu.reset();
         cpu.exe(Some(instructions::NOP_IMP_CCOST));
-        assert_all_status_flags_false_except(&cpu, None);
+        assert_all_status_flags_false_except(&cpu, vec![]);
         assert_eq!(cpu.get_cycles(), instructions::NOP_IMP_CCOST);
     }
 
@@ -265,9 +307,8 @@ mod tests {
         let mut cpu = cpu_mem_set(vec![
             (0xFFFC, instructions::SEC_IMP),
         ]);
-        cpu.reset();
         cpu.exe(Some(instructions::SEC_IMP_CCOST));
-        assert_all_status_flags_false_except(&cpu, Some(Mos6502Flags::C));
+        assert_all_status_flags_false_except(&cpu, vec![Mos6502Flags::C]);
         assert!(cpu.carry_flag());
         assert_eq!(cpu.get_cycles(), instructions::SEC_IMP_CCOST);
     }
@@ -279,9 +320,8 @@ mod tests {
         let mut cpu = cpu_mem_set(vec![
             (0xFFFC, instructions::SEI_IMP),
         ]);
-        cpu.reset();
         cpu.exe(Some(instructions::SEI_IMP_CCOST));
-        assert_all_status_flags_false_except(&cpu, Some(Mos6502Flags::I));
+        assert_all_status_flags_false_except(&cpu, vec![Mos6502Flags::I]);
         assert!(cpu.interrupts_disable_flag());
         assert_eq!(cpu.get_cycles(), instructions::SEI_IMP_CCOST);
     }
@@ -293,9 +333,8 @@ mod tests {
         let mut cpu = cpu_mem_set(vec![
             (0xFFFC, instructions::SED_IMP),
         ]);
-        cpu.reset();
         cpu.exe(Some(instructions::SED_IMP_CCOST));
-        assert_all_status_flags_false_except(&cpu, Some(Mos6502Flags::D));
+        assert_all_status_flags_false_except(&cpu, vec![Mos6502Flags::D]);
         assert!(cpu.decimal_mode_flag());
         assert_eq!(cpu.get_cycles(), instructions::SED_IMP_CCOST);
     }
@@ -307,9 +346,8 @@ mod tests {
         let mut cpu = cpu_mem_set(vec![
             (0xFFFC, instructions::CLI_IMP),
         ]);
-        cpu.reset();
         cpu.exe(Some(instructions::CLI_IMP_CCOST));
-        assert_all_status_flags_false_except(&cpu, None);
+        assert_all_status_flags_false_except(&cpu, vec![]);
         assert_eq!(cpu.get_cycles(), instructions::CLI_IMP_CCOST);
     }
 
@@ -318,10 +356,9 @@ mod tests {
         let mut cpu = cpu_mem_set(vec![
             (0xFFFC, instructions::CLI_IMP),
         ]);
-        cpu.reset();
         cpu.set_status_flag(Mos6502Flags::I);
         cpu.exe(Some(instructions::CLI_IMP_CCOST));
-        assert_all_status_flags_false_except(&cpu, None);
+        assert_all_status_flags_false_except(&cpu, vec![]);
         assert_eq!(cpu.get_cycles(), instructions::CLI_IMP_CCOST);
     }
 
@@ -332,9 +369,8 @@ mod tests {
         let mut cpu = cpu_mem_set(vec![
             (0xFFFC, instructions::CLV_IMP),
         ]);
-        cpu.reset();
         cpu.exe(Some(instructions::CLV_IMP_CCOST));
-        assert_all_status_flags_false_except(&cpu, None);
+        assert_all_status_flags_false_except(&cpu, vec![]);
         assert_eq!(cpu.get_cycles(), instructions::CLV_IMP_CCOST);
     }
 
@@ -343,10 +379,9 @@ mod tests {
         let mut cpu = cpu_mem_set(vec![
             (0xFFFC, instructions::CLV_IMP),
         ]);
-        cpu.reset();
         cpu.set_status_flag(Mos6502Flags::V);
         cpu.exe(Some(instructions::CLV_IMP_CCOST));
-        assert_all_status_flags_false_except(&cpu, None);
+        assert_all_status_flags_false_except(&cpu, vec![]);
         assert_eq!(cpu.get_cycles(), instructions::CLV_IMP_CCOST);
     }
 
@@ -357,9 +392,8 @@ mod tests {
         let mut cpu = cpu_mem_set(vec![
             (0xFFFC, instructions::CLD_IMP),
         ]);
-        cpu.reset();
         cpu.exe(Some(instructions::CLD_IMP_CCOST));
-        assert_all_status_flags_false_except(&cpu, None);
+        assert_all_status_flags_false_except(&cpu, vec![]);
         assert_eq!(cpu.get_cycles(), instructions::CLD_IMP_CCOST);
     }
 
@@ -368,10 +402,9 @@ mod tests {
         let mut cpu = cpu_mem_set(vec![
             (0xFFFC, instructions::CLD_IMP),
         ]);
-        cpu.reset();
         cpu.set_status_flag(Mos6502Flags::D);
         cpu.exe(Some(instructions::CLD_IMP_CCOST));
-        assert_all_status_flags_false_except(&cpu, None);
+        assert_all_status_flags_false_except(&cpu, vec![]);
         assert_eq!(cpu.get_cycles(), instructions::CLD_IMP_CCOST);
     }
 }
